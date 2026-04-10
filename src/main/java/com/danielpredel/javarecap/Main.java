800c685e395b19623af09e93c109e200ccbf4e75
Daniel Preciado Delgadillo <danielpreciadodelgadillo@gmail.com>
2026-04-10 17:53:52 -0600

    Add Loops Class

--- src/main/java/com/danielpredel/javarecap/Main.java
+++ src/main/java/com/danielpredel/javarecap/Main.java
@@ -2,11 +2,15 @@ package com.danielpredel.javarecap;
 
 import com.danielpredel.javarecap.fundamentals.ControlFlowExample;
 import com.danielpredel.javarecap.fundamentals.DataTypesExample;
+import com.danielpredel.javarecap.fundamentals.LoopsExample;
 
 public class Main {
     public static void main(String[] args) {
+
+        //  Fundamentals
         new DataTypesExample().run();
         new ControlFlowExample().run();
+        new LoopsExample().run();
     }
 
 }
