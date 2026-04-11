4157bf6157d4aee527f409adbc8b2bb364fb76bb
Daniel Preciado Delgadillo <danielpreciadodelgadillo@gmail.com>
2026-04-10 22:00:38 -0600

    Add description in Type Inference Class

--- src/main/java/com/danielpredel/javarecap/fundamentals/TypeInference.java
+++ src/main/java/com/danielpredel/javarecap/fundamentals/TypeInference.java
@@ -3,7 +3,7 @@ package com.danielpredel.javarecap.fundamentals;
 public class TypeInference {
 
     // Shows how 'var' can be used for local type inference in modern Java.
-    
+
     public void run() {
         System.out.println("\n=== Type Inference ===");
         exampleTypeInference();
